acfccd090979b0e5b8f02ff3f47986b3bd69e455
Lewis Fang <fang321654@hotmail.com>
2020-04-06 13:03:55 -0700

    MenuScene play button working and slider label working

---
+++ Assets/Scripts/SliderEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
+
+public class SliderEventHandler : MonoBehaviour
+{
+    
+
+    public void test()
+    {
+        TextMeshPro amountLabel = GetComponentInChildren<TextMeshPro>();
+        PinchSlider slider = GetComponentInChildren<PinchSlider>();
+
+        amountLabel.text = System.Math.Floor(slider.SliderValue * 100).ToString();
+    }
+}
