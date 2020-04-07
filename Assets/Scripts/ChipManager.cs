f3cd590088b96ff29bbed567a04821e751585b87
Lewis Fang <fang321654@hotmail.com>
2020-04-07 10:42:26 -0700

    Added a chip spawner object

---
+++ Assets/Scripts/ChipManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class ChipManager : MonoBehaviour
+    {
+        public const string CHIP_PREFAB_FOLDER = "ChipsPrefab";
+        public const string ONE_DOLAR_CHIP = CHIP_PREFAB_FOLDER + "oneDolarChip";
+        public const string FIVE_DOLAR_CHIP = CHIP_PREFAB_FOLDER + "fiveDolarChip";
+        public const string TWENTY_FIVE_DOLAR_CHIP = CHIP_PREFAB_FOLDER + "twentyFiveDolarChip";
+        public const string ONE_HUNDRED_DOLAR_CHIP = CHIP_PREFAB_FOLDER + "oneHundredDolarChip";
+
+        int[] chipSelections = { 1, 5, 25, 100 };
+        int[] chipsNeeded = { 0, 0, 0, 0 };
+
+        public ChipManager()
+        {
+            
+        }
+
+        public List<GameObject> calculateChips(int value)
+        {
+            List<GameObject> chips = null;
+
+            if(value <= 0)
+            {
+                throw new ArgumentException(String.Format("Chip value has to be greater than 0: {0}", value));
+            }
+
+            for(int i = chipSelections.Length; i < 0; i--)
+            {
+                if(chipSelections[i] <= value)
+                {
+                    chipsNeeded[i] = value / chipSelections[i];
+                    value = value % chipSelections[i];
+                }
+            }
+
+            return chips;
+        }
+        
+
+
+    }
+}
---
+++ Assets/SpawnChips.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnChips : MonoBehaviour
+{
+
+    private GameObject chipsObject;
+    private bool spawnChip = true;
+
+    private void Awake()
+    {
+        var chip = Resources.Load("ChipsPrefab/blueChip");
+        chipsObject = chip as GameObject;
+    }
+
+    private void Update()
+    {
+        while (spawnChip)
+        {
+            Spawn();
+            spawnChip = false;
+        }
+        
+    }
+
+    private void Spawn()
+    {
+        Instantiate(chipsObject, transform.position, transform.rotation);
+    }
+}
