81dbb39f1ae8a870e5ca972abbc6a7f5d8966c7b
Lewis Fang <fang321654@hotmail.com>
2020-04-07 14:46:12 -0700

    merged from gamelogic 2 and some fixes

---
+++ Assets/Scripts/CardsPrefabLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class CardsPrefabLoader : MonoBehaviour
+    {
+        private UnityEngine.Object[] cards;
+        private GameObject obj;
+
+        private void Start()
+        {
+            cards = Resources.LoadAll("CardsPrefab", typeof(MeshRenderer));
+
+            foreach (var t in cards)
+            {
+                Debug.Log(t.name);
+            }
+
+            Debug.Log(cards.Length);
+        }
+    }
+}
