3614fbf6928634cf6fd55f0557f947e2eaf757dd
unknown <mrflemchow94@gmail.com>
2020-04-08 13:20:33 -0700

    created new branch for chips

---
+++ Assets/Scripts/BlueChip.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueChip : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject blueChip;
+    [SerializeField]
+    private int chipValue = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject gameChip = Instantiate(blueChip, transform.position, transform.rotation);
+            gameChip.AddComponent<BoxCollider>();
+            gameChip.AddComponent<Rigidbody>();
+            gameChip.AddComponent<NearInteractionGrabbable>();
+            gameChip.AddComponent<ManipulationHandler>();
+            gameChip.AddComponent<TouchHandler>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GreenChip.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenChip : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject greenChip;
+    [SerializeField]
+    private int chipValue = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject gameChip = Instantiate(greenChip, transform.position, transform.rotation);
+            gameChip.AddComponent<BoxCollider>();
+            gameChip.AddComponent<Rigidbody>();
+            gameChip.AddComponent<NearInteractionGrabbable>();
+            gameChip.AddComponent<ManipulationHandler>();
+            gameChip.AddComponent<TouchHandler>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
