66641af7f76b2173b9270207565332d51c82db93
Lewis Fang <fang321654@hotmail.com>
2020-04-08 23:20:05 -0700

    Added Rules scene and exit function

---
+++ Assets/Scripts/ExitGameHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameHandler : MonoBehaviour
+{
+    /// <summary>
+    /// Exit game
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
